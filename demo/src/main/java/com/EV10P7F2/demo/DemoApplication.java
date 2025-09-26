a599134cfc7d867e4dcc1cbbfb2a0381bfb96fde
R1A2H1L1 <117202948+R1A2H1L1@users.noreply.github.com>
2025-09-26 12:02:00 -0500

    Primer commit

---
+++ demo/src/main/java/com/EV10P7F2/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.EV10P7F2.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/EV10P7F2/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EV10P7F2.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
